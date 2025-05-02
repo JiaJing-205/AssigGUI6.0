50e3df8f791d486aeeb9d911a3760ea7affdcbab
AaronOoi2005 <79574231+AaronOoi2005@users.noreply.github.com>
2025-05-02 16:37:44 +0800

    Merge branch 'main' of https://github.com/FlareMix/GUI_Asignment
    # Conflicts:
    #	nbproject/private/private.xml
    #	src/java/item/addItemServlet.java
    #	web/WEB-INF/web.xml
    #	web/addItem.html
    #	web/admin.html
    #	web/displayReport.jsp
    #	web/staff.html
    #	web/user.html
    #	web/viewUser.jsp

--- src/java/report/reportDA.java
+++ src/java/report/reportDA.java
@@ -79,8 +79,15 @@ public class reportDA {
     }
         
     public List<Report> getTop3ItemByMonth(int month) throws SQLException {
-        String query = "SELECT i.itemID, i.itemName, i.itemPrice, i.itemCategory, SUM(o.quantity) AS total_sold, (SUM(o.quantity) * i.itemPrice) AS total_sales FROM Item INNER JOIN OrderInfo o ON i.itemID = o.itemID INNER JOIN Payment p ON o.paymentID = p.paymentID WHERE MONTH(p.paymentDate) = ? GROUP BY i.itemID, i.itemName, i.itemPrice, i.itemCategory order by itemID DESC";
-
+String query = "SELECT i.itemID, i.itemName, i.itemPrice, i.itemCategory, " +
+               "SUM(o.quantity) AS total_sold, " +
+               "SUM(o.quantity * i.itemPrice) AS total_sales " +
+               "FROM Item i " +
+               "INNER JOIN OrderInfo o ON i.itemID = o.itemID " +
+               "INNER JOIN Payment p ON o.paymentID = p.paymentID " +
+               "WHERE MONTH(p.paymentDate) = ? " +
+               "GROUP BY i.itemID, i.itemName, i.itemPrice, i.itemCategory " +
+               "ORDER BY i.itemID DESC";
 
         List<Report> reportList = new ArrayList<>();
         stmt = conn.prepareStatement(query);
@@ -150,8 +157,15 @@ public class reportDA {
     }
          
      public List<Report> getLast3ItemByMonth(int month) throws SQLException {
-        String query = "SELECT i.itemID, i.itemName, i.itemPrice, i.itemCategory, SUM(o.quantity) AS total_sold, (SUM(o.quantity) * i.itemPrice) AS total_sales FROM Item INNER JOIN OrderInfo o ON i.itemID = o.itemID INNER JOIN Payment p ON o.paymentID = p.paymentID WHERE MONTH(p.paymentDate) = ? GROUP BY i.itemID, i.itemName, i.itemPrice, i.itemCategory order by itemID";
-
+    String query = "SELECT i.itemID, i.itemName, i.itemPrice, i.itemCategory, " +
+               "SUM(o.quantity) AS total_sold, " +
+               "SUM(o.quantity * i.itemPrice) AS total_sales " +
+               "FROM Item i " +
+               "INNER JOIN OrderInfo o ON i.itemID = o.itemID " +
+               "INNER JOIN Payment p ON o.paymentID = p.paymentID " +
+               "WHERE MONTH(p.paymentDate) = ? " +
+               "GROUP BY i.itemID, i.itemName, i.itemPrice, i.itemCategory " +
+               "ORDER BY i.itemID";
         List<Report> reportList = new ArrayList<>();
         stmt = conn.prepareStatement(query);
         stmt.setInt(1, month);
